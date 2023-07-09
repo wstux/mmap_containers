f2a08c4141f8f4ad478c815da8b56c2675e6a415
Chistyakov Alexander <wstux1@gmail.com>
2023-07-09 16:10:32 +0300

    + libs/mfcnt: #2 Sceleton for mmap wrapper;

---
+++ libs/mfcnt/details/utils.h
@@ -0,0 +1,35 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2023 Chistyakov Alexander.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _MMAP_CONTAINERS_MFCNT_UTILS_H
+#define _MMAP_CONTAINERS_MFCNT_UTILS_H
+
+namespace mfcnt {
+namespace details {
+
+} // namespace details
+} // namespace mfcnt
+
+#endif /* _MMAP_CONTAINERS_MFCNT_UTILS_H */
+
